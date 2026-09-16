0a2a6ed0f7fbb12fe229fcfdb062a3b6fa96b891
llaef <abdoufakir970@gmail.com>
2026-09-16 05:11:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const { Client } = require('discord.js-selfbot-v13');
 const client = new Client();
 
 client.on('ready', async () => {
-  console.log(`${client.user.username} متصل بنجاح!`);
-  
-  const rpc = new (require('discord.js-selfbot-v13').RichPresence)(client)
-    .setType('PLAYING')
-    .setApplicationId(process.env.APPLICATION_ID || '1276928955563114600')
-    .setName('Minecraft')
-    .setDetails('Playing Bedrock Edition')
-    .setState('Survival Mode (Multiplayer)')
-    .setAssetsLargeImage(process.env.setLargeImageKey('img_0604')
-    .setAssetsLargeText('Minecraft Bedrock')
-    .setStartTimestamp(Date.now());
+    console.log(`${client.user.username} متصل بنجاح!`);
 
-  client.user.setActivity(rpc);
-  console.log("تم تفعيل حالة الماينكرافت بنجاح!");
+    const rpc = new (require('discord.js-selfbot-v13').RichPresence)(client)
+        .setType('PLAYING')
+        .setApplicationId(process.env.APPLICATION_ID)
+        .setName('Minecraft')
+        .setDetails('Playing Bedrock Edition')
+        .setState('Survival Mode (Multiplayer)')
+        .setLargeImageKey(process.env.LARGE_IMAGE_KEY)
+        .setAssetsLargeText('Minecraft Bedrock')
+        .setStartTimestamp(Date.now());
+
+    client.user.setActivity(rpc);
+    console.log("تم تفعيل حالة الماينكرافت بنجاح!");
 });
 
 client.login(process.env.USER_TOKEN);
