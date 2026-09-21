bab87695b4cd36ee42ae16c522e4e680327eb11f
llaef <abdoufakir970@gmail.com>
2026-09-21 11:44:35 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "token": process.env.USER_TOKEN,
+    "mode": "game",
+    "status": "online",
+
+    "game": {
+        "applicationID": process.env.APPLICATION_ID,
+        "name": "League of Legends",
+        "details": "ARAM: Mayhem",
+        "state": "",
+        "party": {
+            "id": "lol_party_xyz",
+            "size": [1, 5]
+        }
+    }
+};
